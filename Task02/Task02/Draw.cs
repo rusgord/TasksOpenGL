e23e7ee409d773b13ef51dbe1c18d84caad8a031
ruslan <rusgord59@gmail.com>
2024-10-22 08:31:13 +0300

    Changed Task2

--- Task02/Task02/Draw.cs
+++ Task02/Task02/Draw.cs
@@ -36,78 +36,40 @@ namespace Task02
                             switch (drawingMode)
                             {
                                 case DrawingMode.Fill:
-                                    glColor3f(255, 0, 0);
-                                    glBegin(GL_TRIANGLE_STRIP);
-                                    CoordinatesTriangleUP(offsetX, offsetY, size);
-                                    glEnd();
-                                    glColor3f(255, 255, 0);
-                                    glBegin(GL_TRIANGLE_STRIP);
-                                    CoordinatesTriangleUP(offsetX + size / 2, offsetY + size, size);
-                                    glEnd();
-                                    glColor3f(255, 255, 255);
-                                    glBegin(GL_TRIANGLE_STRIP);
-                                    CoordinatesTriangleDown(offsetX, offsetY, size);
-                                    glEnd();
-                                    glBegin(GL_TRIANGLE_STRIP);
-                                    CoordinatesTriangleDown(offsetX + size / 2, offsetY + size, size);
-                                    glEnd();
-                                    glColor3f(0, 255, 0);
-                                    glBegin(GL_POLYGON);
-                                    CoordinatesPolygon(offsetX + size, offsetY, size);
-                                    glEnd();
-                                    glColor3f(0, 0, 255);
-                                    glBegin(GL_POLYGON);
-                                    CoordinatesPolygon(offsetX + size * 1.5, offsetY + size, size);
-                                    glEnd();
+                                    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);                                    
                                     break;
                                 case DrawingMode.Line:
-                                    glColor3f(255, 0, 0);
-                                    glBegin(GL_LINE_LOOP);
-                                    CoordinatesTriangleUP(offsetX, offsetY, size);
-                                    glEnd();
-                                    glColor3f(255, 255, 0);
-                                    glBegin(GL_LINE_LOOP);
-                                    CoordinatesTriangleDown(offsetX, offsetY, size);
-                                    glEnd();
-                                    glColor3f(255, 255, 255);
-                                    glBegin(GL_LINE_LOOP);
-                                    CoordinatesTriangleUP(offsetX + size / 2, offsetY + size, size);
-                                    glEnd();
-                                    glBegin(GL_LINE_LOOP);
-                                    CoordinatesTriangleDown(offsetX + size / 2, offsetY + size, size);
-                                    glEnd();
-                                    glColor3f(0, 255, 0);
-                                    glBegin(GL_LINE_LOOP);
-                                    CoordinatesPolygon(offsetX + size, offsetY, size);
-                                    glEnd();
-                                    glColor3f(0, 0, 255);
-                                    glBegin(GL_LINE_LOOP);
-                                    CoordinatesPolygon(offsetX + size * 1.5, offsetY + size, size);
-                                    glEnd();
+                                    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);                                    
                                     break;
                                 case DrawingMode.Point:
-                                    glPointSize(2.0f);
-                                    glBegin(GL_POINTS);
-                                    glColor3f(255, 0, 0);
-                                    CoordinatesTriangleUP(offsetX, offsetY, size);
-                                    glColor3f(255, 255, 0);
-                                    CoordinatesTriangleDown(offsetX, offsetY, size);
-                                    glColor3f(255, 255, 255);
-                                    CoordinatesTriangleUP(offsetX + size / 2, offsetY + size, size);
-                                    CoordinatesTriangleDown(offsetX + size / 2, offsetY + size, size);
-                                    glColor3f(0, 255, 0);
-                                    CoordinatesPolygon(offsetX + size, offsetY, size);
-                                    glColor3f(0, 0, 255);
-                                    CoordinatesPolygon(offsetX + size * 1.5, offsetY + size, size);
-                                    glEnd();
+                                    glPolygonMode(GL_FRONT_AND_BACK, GL_POINT);                                    
                                     break;
                             }
+                            glColor3f(1, 0, 0);
+                            glBegin(GL_TRIANGLES);
+                            CoordinatesTriangleUP(offsetX, offsetY, size);                            
+                            glColor3f(1, 1, 0);                            
+                            CoordinatesTriangleUP(offsetX + size / 2, offsetY + size, size);                            
+                            glColor3f(1, 1, 1);                            
+                            CoordinatesTriangleDown(offsetX, offsetY, size);                            
+                            CoordinatesTriangleDown(offsetX + size / 2, offsetY + size, size);
+                            glEnd();
+                            glColor3f(0, 1, 0);
+                            glBegin(GL_POLYGON);
+                            CoordinatesPolygon(offsetX + size, offsetY, size);
+                            glEnd();
+                            glColor3f(0, 0, 1);
+                            glBegin(GL_POLYGON);
+                            CoordinatesPolygon(offsetX + size * 1.5, offsetY + size, size);
+                            glEnd();
+
                         }
                     }
                 }
             }
             public void DrawButtons(DrawingMode drawingMode, double start_x, double end_x, double start_y, double end_y)
             {
+                glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
                 double buttonWidth = 50.0;
                 double buttonHeight = 25.0;
                 double spacing = 10.0;
