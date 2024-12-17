987dcb44f6de72446cf624efaa24d84eb10cbb76
ruslan <rusgord59@gmail.com>
2024-12-17 08:56:30 +0200

    Task06

---
+++ Task06/Task06/MainForm.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace Task06
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            renderControl1 = new RenderControl();
+            SuspendLayout();
+            // 
+            // renderControl1
+            // 
+            renderControl1.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+            renderControl1.BackColor = System.Drawing.Color.SlateGray;
+            renderControl1.CameraPhi = 1.5707963267948966D;
+            renderControl1.CameraRadius = 10D;
+            renderControl1.CameraTheta = 0D;
+            renderControl1.ForeColor = System.Drawing.Color.White;
+            renderControl1.Location = new System.Drawing.Point(12, 12);
+            renderControl1.Multi = 1D;
+            renderControl1.Name = "renderControl1";
+            renderControl1.PanOffsetX = 0D;
+            renderControl1.PanOffsetY = 0D;
+            renderControl1.Size = new System.Drawing.Size(480, 300);
+            renderControl1.TabIndex = 0;
+            renderControl1.TextCodePage = 1251;
+            renderControl1.MouseDown += OnMoudeDown;
+            renderControl1.MouseMove += OnMouseMove;
+            renderControl1.MouseUp += OnMouseUp;
+            renderControl1.MouseWheel += OnWheel;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(624, 324);
+            Controls.Add(renderControl1);
+            Icon = (System.Drawing.Icon)resources.GetObject("$this.Icon");
+            Name = "MainForm";
+            Text = "Main Form";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private RenderControl renderControl1;
+    }
+}
+
---
+++ Task06/Task06/MainForm.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Windows.Forms;
+using static Task06.OpenGL;
+
+
+namespace Task06
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        private void OnWheel(object sender, MouseEventArgs e)
+        {
+            if (e.Delta < 0)
+            {
+                renderControl1.Multi += 0.1;
+            }
+            else
+            {
+                if (renderControl1.Multi > 0.2)
+                {
+                    renderControl1.Multi -= 0.1;
+                }
+            }
+            renderControl1.Invalidate();
+        }
+        private void OnMoudeDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right && (Control.ModifierKeys & Keys.Control) == Keys.Control)
+            {
+                renderControl1.FigureRotating = true;
+                renderControl1.FigureChanging = false;
+                renderControl1.LastRightMouseX = e.X;
+                renderControl1.NotRotating = true;
+            }
+            if (e.Button == MouseButtons.Left && (Control.ModifierKeys & Keys.Control) == Keys.Control)
+            {
+                renderControl1.FigureChanging = true;
+                renderControl1.LastLeftMouseY = e.Y;
+                renderControl1.NotRotating = true;
+            }
+            else
+            {
+                if (renderControl1.NotRotating && e.Button == MouseButtons.Left)
+                {
+                    renderControl1.NotRotating = false;
+                    renderControl1.FigureChanging = false;
+                    renderControl1.LastLeftMouseX = e.X;
+                    renderControl1.LastLeftMouseY = e.Y;
+                }
+            }
+            renderControl1.Invalidate();
+        }
+
+        private void OnMouseUp(object sender, MouseEventArgs e)
+        {
+
+            if (e.Button == MouseButtons.Right && (Control.ModifierKeys & Keys.Control) == Keys.Control)
+            {
+                renderControl1.FigureRotating = false;
+                renderControl1.FigureChanging = false;
+                renderControl1.NotRotating = true;
+            }
+            if (e.Button == MouseButtons.Left && (Control.ModifierKeys & Keys.Control) == Keys.Control)
+            {
+                renderControl1.FigureChanging = false;
+                renderControl1.FigureRotating = false;
+                renderControl1.NotRotating = true;
+            }
+            else
+            {
+                if (!renderControl1.NotRotating && e.Button == MouseButtons.Left)
+                {
+                    renderControl1.NotRotating = true;
+                }
+            }
+            renderControl1.Invalidate();
+        }
+
+        private void OnMouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right && (Control.ModifierKeys & Keys.Control) == Keys.Control)
+            {
+                if (renderControl1.FigureRotating) 
+                {
+                    double deltaX = (e.X - renderControl1.LastRightMouseX) * 0.5;
+                    renderControl1.RotationAngleY = renderControl1.RotationAngleY - deltaX;
+                    renderControl1.LastRightMouseX = e.X;
+                    renderControl1.Invalidate();
+                }
+            }
+
+            if (e.Button == MouseButtons.Left && (Control.ModifierKeys & Keys.Control) == Keys.Control)
+            {
+                if (renderControl1.FigureChanging)
+                {
+                    double deltaY = (e.Y - renderControl1.LastLeftMouseY) * 0.01;
+                    renderControl1.PointBY = Math.Clamp(renderControl1.PointBY - deltaY, 0.05, 0.48);
+
+                    renderControl1.LastLeftMouseY = e.Y;
+                    renderControl1.Invalidate();
+                }
+            }
+
+            if (!renderControl1.NotRotating)
+            {
+                double deltaX = e.X - renderControl1.LastLeftMouseX;
+                double deltaY = e.Y - renderControl1.LastLeftMouseY;
+
+                renderControl1.CameraTheta += deltaX * 0.01;
+                renderControl1.CameraPhi -= deltaY * 0.01;
+
+                renderControl1.CameraPhi = Math.Clamp(renderControl1.CameraPhi, 0.1, Math.PI - 0.1);
+
+                renderControl1.LastLeftMouseX = e.X;
+                renderControl1.LastLeftMouseY = e.Y;
+
+                renderControl1.Invalidate();
+            }
+        }
+    }
+}
